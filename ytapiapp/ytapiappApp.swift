af99e6a1a62fa1c5f1f0212009881386d3f1805a
notKshen <shenkobe.111@gmail.com>
2024-07-07 20:53:13 -0700

    Initial Commit

---
+++ ytapiapp/ytapiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ytapiappApp.swift
+//  ytapiapp
+//
+//  Created by Kobe Shen on 2024-07-07.
+//
+
+import SwiftUI
+
+@main
+struct ytapiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
