2252c32120ea0ca298f8e99a5c8848db54434bfa
notKshen <shenkobe.111@gmail.com>
2024-08-27 00:25:28 -0700

    development

--- ytapiapp/Views/HomeView.swift
+++ ytapiapp/Views/HomeView.swift
@@ -1,18 +1,15 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  ytapiapp
 //
-//  Created by Kobe Shen on 2024-07-07.
+//  Created by Kobe Shen on 2024-07-22.
 //
 
 import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        TabView {
-            
-        }
-        .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
